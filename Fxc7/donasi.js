2210d6a670ce4375d0e8009dd5df6b4f76280c08
Tersakiti404-cyber <76648497+Tersakiti404-cyber@users.noreply.github.com>
2021-02-07 02:25:09 +0700

    Add files via upload

---
+++ Fxc7/donasi.js
@@ -0,0 +1,24 @@
+const donasi = (Ig, name) => { 
+	return `       
+┏━━━━━━━━━━━━━━━━━━━━
+┃          DONASI  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱ *DONASI SEIKHLASNYA:)* ❉⊰━━✿
+┃  
+┣━⊱ *PULSA*
+┣⊱ 085727091924
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃  *BOT BY ${name}*
+┗━━━━━━━━━━━━━━━━━━━━
+Note:
+Jika Gamau Donasi Setidaknya Follow IG Ngab:D
+Kalo Dah Donasi Silahkan Invit Di Group Kalian:)
+Makasih:)
+
+${Ig}
+
+`
+}
+
+exports.donasi = donasi
